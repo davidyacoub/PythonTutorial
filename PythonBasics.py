727c8c16bb967de222c7fe8240a9422ea34416df
David Yacoub <Davidyacoub58@gmail.com>
2019-09-08 17:06:08 -0700

    Add files via upload

---
+++ PythonBasics.py
@@ -0,0 +1,12 @@
+import sys
+import math
+import random
+import threading
+import time
+from functools import reduce
+
+l1 = [[1, 2], [3, 4]]
+print("1st", l1[1][1])
+
+for x in range(0, 10):
+    print(x)
